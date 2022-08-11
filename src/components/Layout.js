a70ff6b432e1ebe298b93c5ad22321a7d06ed9eb
Firofa <rizifaarsien@gmail.com>
2022-08-11 08:39:31 +0700

    Add Mobile Menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <div className='flex flex-auto'>
-                <Sidebar />
-                <div className='grow'>
-                    <Navbar />
-                    <div className='m-5'>{children}</div>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-auto">
+        <Sidebar />
+        <div className="grow">
+          <Navbar />
+          <div className="lg:ml-64 ml-24 dark:text-white">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,73 +1,73 @@
-import React, { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-import { BsArrowLeftCircle } from 'react-icons/bs'
-import { AiFillPieChart } from 'react-icons/ai'
-import { SiFuturelearn } from 'react-icons/si'
-import { SiOpenaccess } from 'react-icons/si'
-import { CgProfile } from 'react-icons/cg'
-import Logo from '../assets/images/logo.svg'
+import { BsArrowLeftCircle } from "react-icons/bs";
+import { AiFillPieChart } from "react-icons/ai";
+import { SiFuturelearn } from "react-icons/si";
+import { SiOpenaccess } from "react-icons/si";
+import { CgProfile } from "react-icons/cg";
+import Logo from "../assets/images/logo.svg";
 
 const Sidebar = () => {
-    const [open, setOpen] = useState(false)
-    const location = useLocation()
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
 
-    const Menus = [
-        { title: 'Dashboard', path: '/dashboard', src: <AiFillPieChart /> },
-        { title: 'Course', path: '/course', src: <SiFuturelearn /> },
-        { title: 'Profile', path: '/profile', src: <CgProfile /> },
-        { title: 'Signin', path: '/login', src: <SiOpenaccess />, gap: 'true' },
-    ]
+  const Menus = [
+    { title: "Dashboard", path: "/dashboard", src: <AiFillPieChart /> },
+    { title: "Course", path: "/course", src: <SiFuturelearn /> },
+    { title: "Profile", path: "/profile", src: <CgProfile /> },
+    { title: "Signin", path: "/login", src: <SiOpenaccess />, gap: "true" },
+  ];
 
-    return (
-        <>
-            <div
-                className={`${
-                    open ? 'w-60' : 'w-fit'
-                } hidden sm:block relative h-screen duration-300 bg-gray-100 border-r border-gray-200 dark:border-gray-600 p-5 dark:bg-slate-800`}
-            >
-                <BsArrowLeftCircle
-                    className={`${
-                        !open && 'rotate-180'
-                    } absolute text-3xl bg-white fill-slate-800  rounded-full cursor-pointer top-9 -right-4 dark:fill-gray-400 dark:bg-gray-800`}
-                    onClick={() => setOpen(!open)}
-                />
-                <Link to='/'>
-                    <div className={`flex ${open && 'gap-x-4'} items-center`}>
-                        <img src={Logo} alt='' className='pl-2' />
-                        {open && (
-                            <span className='text-xl font-medium whitespace-nowrap dark:text-white'>
-                                Goal Quest
-                            </span>
-                        )}
-                    </div>
-                </Link>
+  return (
+    <>
+      <div
+        className={`${
+          open ? "w-60" : "w-fit"
+        } sm:block absolute min-h-screen h-full duration-300 bg-gray-100 border-r border-gray-200 dark:border-gray-600 p-5 dark:bg-slate-800`}
+      >
+        <BsArrowLeftCircle
+          className={`${
+            !open && "rotate-180"
+          } absolute text-3xl bg-white fill-slate-800  rounded-full cursor-pointer top-9 -right-4 dark:fill-gray-400 dark:bg-gray-800`}
+          onClick={() => setOpen(!open)}
+        />
+        <Link to="/">
+          <div className={`flex ${open && "gap-x-4"} items-center`}>
+            <img src={Logo} alt="" className="pl-2" />
+            {open && (
+              <span className="text-xl font-medium whitespace-nowrap dark:text-white">
+                Goal Quest
+              </span>
+            )}
+          </div>
+        </Link>
 
-                <ul className='pt-6'>
-                    {Menus.map((menu, index) => (
-                        <Link to={menu.path} key={index}>
-                            <li
-                                className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
-                        ${menu.gap ? 'mt-9' : 'mt-2'} ${
-                                    location.pathname === menu.path &&
-                                    'bg-gray-200 dark:bg-gray-700'
-                                }`}
-                            >
-                                <span className='text-2xl'>{menu.src}</span>
-                                <span
-                                    className={`${
-                                        !open && 'hidden'
-                                    } origin-left duration-300 hover:block`}
-                                >
-                                    {menu.title}
-                                </span>
-                            </li>
-                        </Link>
-                    ))}
-                </ul>
-            </div>
-        </>
-    )
-}
+        <ul className="pt-6">
+          {Menus.map((menu, index) => (
+            <Link to={menu.path} key={index}>
+              <li
+                className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                        ${menu.gap ? "mt-9" : "mt-2"} ${
+                  location.pathname === menu.path &&
+                  "bg-gray-200 dark:bg-gray-700"
+                }`}
+              >
+                <span className="text-2xl">{menu.src}</span>
+                <span
+                  className={`${
+                    !open && "hidden"
+                  } origin-left duration-300 hover:block`}
+                >
+                  {menu.title}
+                </span>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
